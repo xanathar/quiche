35a2c7e9e0d2e2c65fa3ed9d0bac5becacac051c
Marco Mastropaolo <xanathar@users.noreply.github.com>
2020-04-01 14:52:13 +0200

    Added C interface to datagram API

--- include/quiche.h
+++ include/quiche.h
@@ -161,6 +161,9 @@ void quiche_config_set_ack_delay_exponent(quiche_config *config, uint64_t v);
 // Sets the `max_ack_delay` transport parameter.
 void quiche_config_set_max_ack_delay(quiche_config *config, uint64_t v);
 
+// Sets the `max_datagram_frame_size` transport parameter.
+void quiche_config_set_max_datagram_frame_size(quiche_config *config, uint64_t v);
+
 // Sets the `disable_active_migration` transport parameter.
 void quiche_config_set_disable_active_migration(quiche_config *config, bool v);
 
@@ -230,6 +233,18 @@ ssize_t quiche_conn_stream_recv(quiche_conn *conn, uint64_t stream_id,
 // Writes data to a stream.
 ssize_t quiche_conn_stream_send(quiche_conn *conn, uint64_t stream_id,
                                 const uint8_t *buf, size_t buf_len, bool fin);
+
+// Reads a received datagram
+ssize_t quiche_conn_dgram_recv(quiche_conn *conn, uint8_t **out);
+
+// Frees memory occupied by a received datagram
+void quiche_conn_dgram_free(quiche_conn *conn, uint8_t *dgram,
+                            size_t dgram_len);
+
+// Sends a datagram
+ssize_t quiche_conn_dgram_send(quiche_conn *conn, const uint8_t *buf,
+                               size_t buf_len);
+
 
 enum quiche_shutdown {
     QUICHE_SHUTDOWN_READ = 0,
