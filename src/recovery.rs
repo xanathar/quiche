235bd789c9350ca5a572a109f01df839dcb720e3
Marco Mastropaolo <marcomp@amazon.com>
2020-05-27 15:48:32 +0200

    Version with disabled CC for testing

--- src/recovery.rs
+++ src/recovery.rs
@@ -676,6 +676,8 @@ pub enum CongestionControlAlgorithm {
     Reno  = 0,
     /// CUBIC congestion control algorithm (default). `cubic` in a string form.
     CUBIC = 1,
+    /// NoCC
+    NoCC = 2,
 }
 
 impl FromStr for CongestionControlAlgorithm {
@@ -715,6 +717,7 @@ impl From<CongestionControlAlgorithm> for &'static CongestionControlOps {
         match algo {
             CongestionControlAlgorithm::Reno => &reno::RENO,
             CongestionControlAlgorithm::CUBIC => &cubic::CUBIC,
+            CongestionControlAlgorithm::NoCC => &nocc::NoCC,
         }
     }
 }
@@ -1253,3 +1256,4 @@ mod cubic;
 mod delivery_rate;
 mod hystart;
 mod reno;
+mod nocc;
